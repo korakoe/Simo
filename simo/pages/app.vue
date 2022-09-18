cb5b878acdcfc2b3d54d413331fbc042132b4e5d
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-09-18 17:40:03 +0800

    begin PWA

---
+++ simo/pages/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Navbar from "./components/layout/navbar.vue";
+</script>
+
+<template>
+    <head>
+        <link rel="manifest" href="manifest.json" />
+    </head>
+    <body class="bg-main-2-light dark:bg-main-2-dark transition duration-150 ease-in-out">
+        <Navbar>
+            Hello World
+        </Navbar>
+    </body>
+</template>
--- simo/pages/index.vue
+++ simo/pages/index.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-import Navbar from "./components/layout/navbar.vue";
-</script>
-
 <template>
     <body class="bg-main-2-light dark:bg-main-2-dark transition duration-150 ease-in-out">
-        <Navbar>
             Hello World
-        </Navbar>
     </body>
 </template>
