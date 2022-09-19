88a2d31e6667579b6fc0687df0fa5a1b84b1e787
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-09-19 20:01:21 +0800

    see?

--- simo/pages/components/layout/navbar.vue
+++ simo/pages/components/layout/navbar.vue
@@ -2,10 +2,10 @@
     <header>    
         <div class="navbar-mobile sm:drop-shadow-md bg-gradient-to-t from-compliment-light to-compliment-2-light dark:bg-gradient-to-b dark:from-compliment-dark dark:to-compliment-2-dark sm:navbar-desktop sm:from-main1-light sm:to-main1-light dark:sm:from-main1-dark dark:sm:to-main1-dark transition duration-150 ease-in-out">
             <div class="nav-icon-container">
-                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="40" height="40">
-                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="40" height="40">
-                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="40" height="40">
-                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="40" height="40">
+                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="30" height="30">
+                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="30" height="30">
+                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="30" height="30">
+                <img class="nav-icon-light dark:nav-icon-dark" src="~/assets/images/group.svg" width="30" height="30">
             </div>
         </div>
     </header>
