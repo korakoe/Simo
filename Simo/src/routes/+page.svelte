17f2ace440185b0b027006e2c413a885303f2eee
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-01-14 13:05:04 +0800

    Start working on landing page

--- Simo/src/routes/+page.svelte
+++ Simo/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class=LandingTitle>Simo.</h1>
+<span class=LandingWhite>The <span class=LandingBlue>Simon Says </span> social media</span>
+
+<style>
+    .LandingTitle {
+        color: #FF008A;
+        font-family: 'Reem Kufi Ink', sans-serif;
+        font-weight: bold;
+    }
+
+    .LandingWhite {
+        color: white;
+        font-family: 'Reem Kufi Ink', sans-serif;
+        font-weight: bold;
+    }
+
+    .LandingBlue {
+        color: #20A4F3;
+        font-family: 'Reem Kufi Ink', sans-serif;
+        font-weight: bold;
+    }
+</style>
