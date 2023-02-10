3f7c68bbca4a8cf28f29bde0b782a15ce357e45b
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-02-10 18:03:50 +0800

    start log in page

--- Simo/src/routes/+layout.svelte
+++ Simo/src/routes/+layout.svelte
@@ -1,5 +1,37 @@
 <script>
     import "../app.css";
+    import { browser } from '$app/environment';
+
+let darkMode = true;
+
+function handleSwitchDarkMode() {
+    darkMode = !darkMode;
+
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+
+    darkMode
+        ? document.documentElement.classList.add('dark')
+        : document.documentElement.classList.remove('dark');
+}
+
+
+
+if (browser) {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        handleSwitchDarkMode();
+    });
+    if (
+        localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+        document.documentElement.classList.add('dark');
+        darkMode = true;
+    } else {
+        document.documentElement.classList.remove('dark');
+        darkMode = false;
+    }
+}
 </script>
 
 <slot />
+
---
+++ Simo/src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import DarkToggle from "$lib/darkToggle.svelte";
+</script>
+
+<DarkToggle />
+
+<div class="login-card dark:login-card-dark">
+    <h1 class="Large-Red-Centered">Log-in</h1>
+</div>
+
