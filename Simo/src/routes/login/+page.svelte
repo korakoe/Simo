08070f7541cd927d2de7313bf08745d70ac3f425
ButterCream <56580073+korakoe@users.noreply.github.com>
2023-07-12 21:36:39 +0800

    add no select to login

--- Simo/src/routes/login/+page.svelte
+++ Simo/src/routes/login/+page.svelte
@@ -12,17 +12,17 @@
             await applyAction(result)
         }
     }}>
-    <h1 class="Large-Red-Centered">Log-in</h1>
+    <h1 class="Large-Red-Centered no-select">Log-in</h1>
     <h2 class="Medium-Blue-Centered"> It's nice to see you again!</h2>
-    <hr class="Red-line">
+    <hr class="Red-line no-select">
     <div class="Login-form-card">
-        <h2 class="Small-Text dark:Dark-Small-Text"> Email or Username</h2>
+        <h2 class="Small-Text dark:Dark-Small-Text no-select"> Email or Username</h2>
         <input type="text" class="form-field" name="email">
-        <h2 class="Small-Text dark:Dark-Small-Text"> Password</h2>
+        <h2 class="Small-Text dark:Dark-Small-Text no-select"> Password</h2>
         <input type="password" class="form-field" name=password>
         <div class="button-holder">
-            <input type="submit" value="Continue" class="form-submit">
-            <a class="form-signup" href="/register">Register</a>
+            <input type="submit" value="Continue" class="form-submit no-select">
+            <a class="form-signup no-select" href="/register">Register</a>
         </div>
     </div>
 </form>
