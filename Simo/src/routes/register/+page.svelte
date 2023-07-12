eb529cd8d4e6e7eca8cff3cf772a7c7b7fc515a5
ButterCream <56580073+korakoe@users.noreply.github.com>
2023-07-12 21:38:31 +0800

    add no select to signup

--- Simo/src/routes/register/+page.svelte
+++ Simo/src/routes/register/+page.svelte
@@ -14,21 +14,21 @@
         await applyAction(result)
     }
 }}>
-        <h1 class="Large-Red-Centered">Register</h1>
-        <h2 class="Medium-Blue-Centered"> Woah! A new User!</h2>
-        <hr class="Red-line">
+        <h1 class="Large-Red-Centered no-select">Register</h1>
+        <h2 class="Medium-Blue-Centered no-select"> Woah! A new User!</h2>
+        <hr class="Red-line no-select">
         <div class="Login-form-card">
-            <h2 class="Small-Text dark:Dark-Small-Text"> Username</h2>
+            <h2 class="Small-Text dark:Dark-Small-Text no-select"> Username</h2>
             <input type="text" name="username" class="form-field">
-            <h2 class="Small-Text dark:Dark-Small-Text"> Email</h2>
+            <h2 class="Small-Text dark:Dark-Small-Text no-select"> Email</h2>
             <input type="email" name="email" class="form-field">
-            <h2 class="Small-Text dark:Dark-Small-Text"> Password</h2>
+            <h2 class="Small-Text dark:Dark-Small-Text no-select"> Password</h2>
             <input type="password" name="password" class="form-field">
-            <h2 class="Small-Text dark:Dark-Small-Text"> Confirm Password</h2>
+            <h2 class="Small-Text dark:Dark-Small-Text no-select"> Confirm Password</h2>
             <input type="password" name="passwordConfirm" class="form-field">
             <div class="button-holder">
-                <input type="submit" value="Continue" class="form-submit" formnovalidate>
-                <a class="form-signup" href="/login">Log-in</a>
+                <input type="submit" value="Continue" class="form-submit no-select" formnovalidate>
+                <a class="form-signup no-select" href="/login">Log-in</a>
             </div>
             
         </div>
