a6a148b2a20fbc93bc03a7fef339da08c48e090a
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-07-12 16:45:50 +0800

    add page transitions
    need to fix height for form

---
+++ Simo/src/routes/register/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { fly } from 'svelte/transition'
+  
+    export let data
+  </script>
+  
+  {#key data.url}
+    <div class="transition_container"
+      in:fly={{ x: 200, duration: 300, delay: 500 }}
+      out:fly={{ x: 200, duration: 300 }}
+    >
+      <slot />
+    </div>
+  {/key}
